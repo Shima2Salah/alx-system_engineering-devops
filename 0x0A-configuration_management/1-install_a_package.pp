15211c4f7c31bf0ec6d25e7272dd0f9be31253c0
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-23 21:40:43 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
+  command => 'python3 -m pip install Flask==2.1.0',
   path    => ['/usr/local/bin', '/usr/bin'],
-  unless  => 'flask --version | grep  2.1.0',
+  unless  => 'python3 -m pip show Flask | grep   2.1.0',
 }
