c81c4a94b75c0cf4722bbd9220c207d27077a719
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-23 21:42:22 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# To install flask from pip3
-exec { 'install_flask':
-  command => 'python3 -m pip install Flask==2.1.0',
-  path    => ['/usr/local/bin', '/usr/bin'],
-  unless  => 'python3 -m pip show Flask | grep   2.1.0',
+#Install puppet-lint.
+package { 'puppet-lint':
+  ensure   => '1.1.0',
+  provider => 'gem',
 }
