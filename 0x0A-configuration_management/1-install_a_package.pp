b1ddb6744de6ae7e4402a0012b4da354a02e4ae0
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-23 22:16:57 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# Install pip3
-package { 'python3-pip':
-  ensure => installed,
-}
+# install flask
 
-# Install Flask 2.1.0 and Werkzeug 2.1.1
-exec { 'install_flask_werkzeug':
-  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.1',
-  require => Package['python3-pip'],
+exec { 'flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
 }
