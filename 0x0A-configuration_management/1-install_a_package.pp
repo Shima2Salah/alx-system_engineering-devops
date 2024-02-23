5917f773f56d260d72b82b650e104a17fbfaa36a
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-02-23 22:16:00 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
-#Install puppet-lint.
+# Install pip3
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# Install Flask 2.1.0 and Werkzeug 2.1.1
+exec { 'install_flask_werkzeug':
+  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   require => Package['python3-pip'],
 }
