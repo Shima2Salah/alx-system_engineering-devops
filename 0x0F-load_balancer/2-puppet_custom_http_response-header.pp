eba3d1638b875a35c13816c413ac35df2c4bdb03
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-08 19:21:09 +0200

    Update 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,13 +1,19 @@
-include stdlib
-package { 'nginx':
-  ensure => installed
+#puppet advance
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
-file_line {'header':
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server {',
-  line   => 'add_header X-Served-By $hostname;'
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
-service {'nginx':
-  ensure  => running
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
