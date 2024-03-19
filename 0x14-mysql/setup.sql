f56df68e7bf81763090e14773370815aba46c31f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-19 17:18:47 +0200

    setup.sql

--- 0x14-mysql/setup.sql
+++ 0x14-mysql/setup.sql
@@ -4,13 +4,16 @@ mysql -u root -p
 #write this commands
 CREATE USER holberton_user@localhost IDENTIFIED BY "projectcorrection280hbtn";
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+________________________________________________________
 CREATE DATABASE tyrell_corp;
 USE tyrell_corp;
 CREATE TABLE nexus6(id INTEGER, name TEXT);
 INSERT INTO nexus6 VALUES (0, "Jarvis");
 GRANT SELECT ON tyrell_corp.nexus6 TO holberton_user@localhost;
+_______________________________________________________
 CREATE USER replica_user@'%' IDENTIFIED BY "replica_user";
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user TO holberton_user@localhost;
-CREATE USER web02@34.227.101.160 IDENTIFIED BY "web02";
-GRANT REPLICATION SLAVE ON *.* TO web02@34.227.101.160;
+_______________________________________________________
+CREATE USER web02@34.239.207.152 IDENTIFIED BY "web02";
+GRANT REPLICATION SLAVE ON *.* TO web02@34.239.207.152;
