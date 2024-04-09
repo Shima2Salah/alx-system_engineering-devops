5303a2b39634b54e87b130dcfe487416ff501914
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-04-09 21:11:34 +0200

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" Queries Reddit API and returns number of subscribers for a subreddit. """
+"""get a lit of suscribber on a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    URL = "https://www.reddit.com/r/{}/about.json"
-    Headers = {"User-Agent": "Custom"}
-    response = requests.get(URL.format(subreddit), headers=Headers)
-    if response.status_code != 200:
-        return 0
-    return response.json().get('data').get('subscribers', 0)
+    """number of suscribers"""
+    if subreddit is None or type(subreddit) != str:
+        return (0)
+    url = "http://www.reddit.com/r/{}/about.json".format(
+            subreddit)
+    headers = {'User-Agent': 'API project'}
+    response = requests.get(url, headers=headers)
+    if (response.status_code != 200):
+        return (0)
+    sus = response.json().get("data").get('subscribers', 0)
+    return (sus)
