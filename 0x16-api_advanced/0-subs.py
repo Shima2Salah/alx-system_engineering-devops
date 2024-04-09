49866482dc1e68f7d04867ee81274aedde9d2cca
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-04-09 21:43:33 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,9 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    URL = "https://www.reddit.com/r/{}/about.json"
-    Headers = {"User-Agent": "Custom"}
-    response = requests.get(URL.format(subreddit), headers=Headers)
-    if response.status_code != 200:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}  # Custom User-Agent header
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    return response.json().get('data').get('subscribers', 0)
