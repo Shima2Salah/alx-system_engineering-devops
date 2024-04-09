fc33d78112a00d51695677243cd5bc7ecd21f26e
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-04-09 21:47:22 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and prints the titles of the first 10 hot posts. """
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """ Gets the top ten. """
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10"
-    Headers = {"User-Agent": "Custom"}
-    response = requests.get(URL.format(subreddit), allow_redirects=False,
-                            headers=Headers)
-    if response.status_code != 200:
-        print("None")
-        return
-    hot = response.json().get('data').get('children')
-    if not hot:
-        print("None")
-        return
-    for post in hot:
-        print(post.get('data').get('title'))
+    """ Returns: top ten post titles
+        or None if queried subreddit is invalid """
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+
+    if response.status_code == 200:
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
+    else:
+        print(None)
