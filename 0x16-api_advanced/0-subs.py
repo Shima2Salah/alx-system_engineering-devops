824e9e2ef28f7ec3b321b7c1d66683e0915ded2b
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-04-09 21:26:03 +0200

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
+"""get a lit of suscribber on a subreddit"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     URL = "https://www.reddit.com/r/{}/about.json"
     # Use a more descriptive User-Agent string
-    Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64\
+               ) AppleWebKit/537.36 (KHTML, like Gecko\
+               ) Chrome/58.0.3029.110 Safari/537.3"}
     try:
         response = requests.get(URL.format(subreddit), headers=Headers)
         if response.status_code != 200:
